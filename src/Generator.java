f349bfd95331d99957eb751b378c5ec6ad86c96f
ibogi <ibogino@yandex.ru>
2020-09-22 16:25:28 +0300

    writting with nio added and letters changed into final variable

--- src/Generator.java
+++ src/Generator.java
@@ -1,5 +1,10 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 
@@ -8,6 +13,7 @@ public class Generator implements Runnable {
 
     private CountDownLatch cdl;
     private ConcurrentLinkedQueue<Integer> regions;
+    private static final char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
 
     public Generator(CountDownLatch countDownLatch, ConcurrentLinkedQueue<Integer> regions){
         cdl = countDownLatch;
@@ -19,37 +25,50 @@ public class Generator implements Runnable {
     @Override
     public void run() {
 
-        while (!regions.isEmpty()){
-            String region = regions.poll().toString();
-            PrintWriter writer = null;
-            try {
-                writer = new PrintWriter("res/" + region + ".txt");
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            writer.write(generateNumber(region));
-            cdl.countDown();
-            writer.flush();
-            writer.close();
+//        while (!regions.isEmpty()){
+//            String region = regions.poll().toString();
+//            PrintWriter writer = null;
+//            try {
+//                writer = new PrintWriter("res/" + region + ".txt");
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            }
+//            writer.write(generateNumber(region));
+//            cdl.countDown();
+//            writer.flush();
+//            writer.close();
+//        }
+        while (true){
+        Integer region = regions.poll();
+        cdl.countDown();
+        if (region == null){
+            return;
+        }
+
+        Path p = Paths.get("res/",region + ".txt");
+        try {
+            Files.write(p,generateNumber(region.toString()).getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
         }
 
     }
 
     private static String generateNumber(String reg){
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
         StringBuilder builder = new StringBuilder();
         for(int number = 1; number < 1000; number++) {
             String carNumber = padNumber(number, 3);
             for (char firstLetter : letters) {
                 for (char secondLetter : letters) {
                     for (char thirdLetter : letters) {
-                        builder.append(firstLetter);
-                        builder.append(carNumber);
-                        builder.append(secondLetter);
-                        builder.append(thirdLetter);
-                        builder.append(reg);
-                        builder.append("\n");
+                        builder.append(firstLetter)
+                                .append(carNumber)
+                                .append(secondLetter)
+                                .append(thirdLetter)
+                                .append(reg).append("\n");
 
                     }
                 }
--- src/Loader.java
+++ src/Loader.java
@@ -6,8 +6,7 @@ import java.util.concurrent.*;
 
 public class Loader
 {
-    private static CountDownLatch cdl;
-    private static Generator generator;
+
     public static void main(String[] args) throws Exception
     {
 
@@ -15,17 +14,18 @@ public class Loader
         ConcurrentLinkedQueue<Integer> regionCodes = new ConcurrentLinkedQueue<>();
 
         int region = 0;
-        for (int i = 0; i < 100; i++){
-            regionCodes.add(++region);
+        for (int i = 1; i <= 100; i++){
+            regionCodes.add(i);
+            region++;
         }
 
-        cdl = new CountDownLatch(region);
+        CountDownLatch cdl = new CountDownLatch(region);
 
-        generator = new Generator(cdl, regionCodes);
+        Generator generator = new Generator(cdl, regionCodes);
 
         long start = System.currentTimeMillis();
 
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < 8; i++){
             es.execute(new Thread(generator));
         }
 
@@ -35,36 +35,39 @@ public class Loader
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
 
-        testThreads(1);
-        testThreads(2);
-        testThreads(4);
-        testThreads(8);
-        testThreads(16);
-        testThreads(32);
+
 
     }
 
-    private static void testThreads(int count){
-
-        ExecutorService es = Executors.newFixedThreadPool(count);
-
-        long start = System.currentTimeMillis();
-
-        for (int i = 0; i < count; i++){
-            es.execute(new Thread(generator));
-        }
-
-        try {
-            cdl.await();
-            es.shutdown();
-            System.out.println((System.currentTimeMillis() - start) + " ms");
-
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-            }
+//    private static void testThreads(int count){
+//
+//        ExecutorService es = Executors.newFixedThreadPool(count);
+//
+//        CountDownLatch cdl = new CountDownLatch(regionCodes.size());
+//
+//        Generator generator = new Generator(cdl, regionCodes);
+//
+//        long start = System.currentTimeMillis();
+//
+//        for (int i = 0; i < count; i++){
+//            es.execute(new Thread(generator));
+//        }
+//
+//        try {
+//            cdl.await();
+//            es.shutdown();
+//            if (count == 1){
+//                System.out.println("Count threads: " + count + " - " + (System.currentTimeMillis() - start) + " ms");}
+//            else {
+//                System.out.println("Count threads: " + count + " - " + (System.currentTimeMillis() - start) + " ms");
+//            }
+//
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//            }
 
 
 }
